0b1a442baed94fc7ff79020de3e11071460bd1d6
Sourabh Rajwade <rajwadesourabh@gmail.com>
2021-06-01 22:07:15 +0530

    cart-context added

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,11 +1,31 @@
 import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
 const MealItemForm = (props) => {
+    const [amountIsValid, setAmountValid] = useState(true);
+    const amountInputRef = useRef();
+    const submitHandler = event => {
+        event.preventDefault();
+
+        const enteredAmount = amountInputRef.current.value;
+        const enteredAmountNumber = +enteredAmount;
+
+        if (enteredAmount.trim().length === 0 ||
+            enteredAmountNumber < 1 ||
+            enteredAmountNumber > 5 
+            ) {
+                setAmountValid(false);
+                return;
+            }
+        props.onAddToCart(enteredAmountNumber);
+    }
     return (
-        <form  className={classes.form}>
-            <Input label="Amount" input={{
+        <form  className={classes.form}  onSubmit={submitHandler}>
+            <Input 
+                ref={amountInputRef}
+                label="Amount" 
+                input={{
                 id: 'amount'+ props.id,
                 type: 'number',
                 min: '1',
@@ -14,6 +34,7 @@ const MealItemForm = (props) => {
                 defaultValue: '1'
             }} />
             <button>+ Add</button>
+            {!amountIsValid && <p>Please enter a valid amount(1-5).</p>}
         </form>
     )
 }
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,44 +1,48 @@
-import React, {useReducer} from 'react'
-import CartContext from './cart-context';
+import React, { useReducer } from "react";
+import CartContext from "./cart-context";
 
 const defaultCartState = {
-    items: [],
-    amount: 0
-}
+  items: [],
+  amount: 0,
+};
 
 const cartReducer = (state, action) => {
-    if (action.type === 'ADD_ITEM') {
-        const updatedItems = state.items.concat(action.item);
-        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
-        return {
-            items: updatedItems,
-            totalAmount: updatedTotalAmount
-        }
-    }
-    return defaultCartState;
-}
+  if (action.type === "ADD_ITEM") {
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+  return defaultCartState;
+};
 
 const CartProvider = (props) => {
-    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState)
-    const addItemToCartHandler = (item) => {
-        dispatchCartAction({type: 'ADD_ITEM', payload: item})
-    };
-    const removeItemToCartHandhler = (id) => {
-        dispatchCartAction({type: 'REMOVE_ITEM', payload: id})
-    };
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: "ADD_ITEM", item: item });
+  };
+  const removeItemToCartHandhler = (id) => {
+    dispatchCartAction({ type: "REMOVE_ITEM", id: id });
+  };
 
-    const cartContext = {
-        items: cartState.items,
-        totalAmount: cartState.totalAmount,
-        addItem: addItemToCartHandler,
-        removeItem: removeItemToCartHandhler 
-    };
+  const cartContext = {
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemToCartHandhler,
+  };
 
-    return (
-        <CartContext.Provider value={cartContext}>
-            {props.children}
-        </CartContext.Provider>
-    )
-}
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
+};
 
-export default CartProvider
+export default CartProvider;
